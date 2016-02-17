de4c867681475b3a54405653dfee2c2bc6b3a417
Robin Malhotra <malhotra.robin@icloud.com>
2016-02-18 01:59:57 +0530

    done with stage 3

--- scripts/app.js
+++ scripts/app.js
@@ -5,4 +5,16 @@ angular.module("todoListApp", [])
     {
         console.log("Hello There!!");
     }
+    $scope.todos = [
+        {"name":"clean the house"},
+        {"name":"water the dog"},
+        {"name":"feed the lawn"},
+        {"name":"pay dem bills"},
+        {"name":"run"},
+        {"name":"swim"}
+    ]
+    $scope.learningNgChange = function ()
+    {
+
+    }
 });
