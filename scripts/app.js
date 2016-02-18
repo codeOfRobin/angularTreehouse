f693ef7a6b0d8558939f5a22f66c3a83edb8f91f
Robin Malhotra <malhotra.robin@icloud.com>
2016-02-19 01:15:20 +0530

    so, more stages

--- scripts/app.js
+++ scripts/app.js
@@ -1,20 +1,28 @@
 angular.module("todoListApp", [])
-.controller('mainCtrl',function ($scope)
+.controller('mainCtrl',function ($scope,dataService)
 {
+    $scope.helloConsole = dataService.helloConsole
     $scope.helloWorld = function()
     {
         console.log("Hello There!!");
     }
-    $scope.todos = [
-        {"name":"clean the house"},
-        {"name":"water the dog"},
-        {"name":"feed the lawn"},
-        {"name":"pay dem bills"},
-        {"name":"run"},
-        {"name":"swim"}
-    ]
+
     $scope.learningNgChange = function ()
     {
 
+    }
+    dataService.getTodos(function (response) {
+        console.log(response.data);
+        $scope.todos = response.data
+    })
+})
+.service('dataService',function($http)
+{
+    this.helloConsole = function()
+    {
+        console.log("teri toh")
+    }
+    this.getTodos = function(callback){
+        $http.get('todos.json').then(callback)
     }
 });
